17c51e6a7418e3159b6d62e2c32f34b2b41e8b2d
Daniel Herbolt <dherbolt@kerio.com>
2017-03-13 12:04:41 +0100

    + getPageWithContent, cfg file

--- main.js
+++ main.js
@@ -1,12 +1,33 @@
 var request = require('request');
+var fs = require('fs');
 var accessToken = null;
 var refreshToken = null;
+var tokenType = null;
 var session = null;
 var token = null;
 var reqId = 1;
+var tenant, user;
 
-request({
-		uri: "https://samepage.io/login/oauth/token",
+
+function sendXhr(method, params, callback) {
+	var
+		headers = {
+			'Content-Type': 'application/json'
+		},
+		uri;
+
+	if (method !== 'Session.create') {
+		uri = 'https://samepage.io/api/app/';
+		headers['Authorization'] = tokenType + ' ' + accessToken;
+	}
+	else {
+		uri = 'https://samepage.io/login/api/';
+	}
+
+	uri = uri + 'jsonrpc?method=' + method;
+
+	request({
+		uri: uri,
 		method: 'POST',
 		timeout: 10000,
 		followRedirect: true,
@@ -14,21 +35,21 @@ request({
 		maxRedirects: 10,
 		jar: true,
 		gzip: true,
-		form: {
-			'grant_type': 'password',
-			'device_id': 'web-cbcb4c68e7e2ca8b47bd84d57d3f0f1ebb398ac68832d4c2bba1',
-			'device_name': 'Chrome',
-			'client_id': '8908213257800763793',
-			'username': 'dherbolt@kerio.com',
-			'password': 'aaa123',
-			'scope': 'client'
+		headers: headers,
+		json: {
+			id: reqId++,
+			jsonrpc: "2.0",
+			method: method,
+			params: params
 		}
-	}, function(error, response, body) {
-		accessToken = JSON.parse(body).access_token;
-		refreshToken = JSON.parse(body).refresh_token;
+	}, function (error, response, body) {
+		callback(error, response, body);
+	});
+}
 
-		request({
-			uri: "https://samepage.io/login/api/jsonrpc?method=Session.create",
+function doLogin(userName, password, callback) {
+	request({
+			uri: "https://samepage.io/login/oauth/token",
 			method: 'POST',
 			timeout: 10000,
 			followRedirect: true,
@@ -36,22 +57,55 @@ request({
 			maxRedirects: 10,
 			jar: true,
 			gzip: true,
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			json: {
-				id: reqId++,
-				jsonrpc: "2.0",
-				method: "Session.create",
-				params: {accessToken: refreshToken}
+			form: {
+				'grant_type': 'password',
+				'device_id': 'web-cbcb4c68e7e2ca8b47bd84d57d3f0f1ebb398ac68832d4c2bba1',
+				'device_name': 'Chrome',
+				'client_id': '8908213257800763793',
+				'username': userName,
+				'password': password,
+				'scope': 'client'
 			}
 		}, function (error, response, body) {
-			var result = body.result;
+			var body = JSON.parse(body);
 
-			token = result.token;
-			session = result.session;
+			accessToken = body.access_token;
+			refreshToken = body.refresh_token;
+			tokenType = body.token_type;
 
-			console.log(token + '/' + session);
+			sendXhr('Session.create', {accessToken: refreshToken}, callback);
+		});
+}
+
+function bootstrap(callback) {
+	sendXhr('Bootstrap.bootstrap', {
+		branch: "production",
+		referral: ""
+	}, callback);
+}
+
+function getPageContent(pageId, callback) {
+	sendXhr('Pages.getWithContent', {
+		id: pageId,
+		tenantId: tenant.publicId
+	}, callback);
+}
+
+
+function run() {
+	var cfg = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+
+	doLogin(cfg.sio.userName, cfg.sio.password, function () {
+		bootstrap(function (error, response, body) {
+			tenant = body.result.bootstrapData.tenant;
+			user = body.result.bootstrapData.user;
+
+			getPageContent('58309038387064065', function (error, response, body) {
+				var page = body.result.page;
+				console.log(page);
+			});
 		});
 	});
+}
 
+run();
