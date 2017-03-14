cf2efd29daacf8b7b67c49a347b8a2ca8527e828
Vaclav Purchart <vpurchart@kerio.com>
2017-03-14 11:51:53 +0100

    sorting components according to layout & order

--- importers/sio/sortChildren.js
+++ importers/sio/sortChildren.js
@@ -4,7 +4,27 @@ function sortChildren(page) {
 }
 
 function sortChildrenInternal(node) {
-	return node.children;
+	let columns = {};
+	for (let child of node.children) {
+		let layout = child.layout;
+		columns[layout.column] = columns[layout.column] || [];
+		columns[layout.column][layout.position] = child;
+	}
+
+	let children = [];
+	let knownColumns = ['left', 'center', 'right'];
+
+	for (let columnName in columns) {
+		//console.log('layout column: ' + columnName);
+		console.assert(knownColumns.indexOf(columnName) >= 0);
+		//let items = columns[columnName];
+	}
+
+	for (let columnName of knownColumns) {
+		children = children.concat(columns[columnName]);
+	}
+
+	return children;
 }
 
 module.exports = sortChildren;
--- importers/sio/test.js
+++ importers/sio/test.js
@@ -1,5 +1,6 @@
 const jetpack = require('fs-jetpack');
 const path = require('path');
+const sortChildren = require(__dirname + '/sortChildren');
 
 let pageRootPath = path.normalize(__dirname + '/../../download/10k-users-in-kerio-connect--58309038387064065');
 let outPath = pageRootPath + '/page-sorted.json';
@@ -7,5 +8,7 @@ const pageJson = pageRootPath + '/page.json';
 
 console.log(`> Reading ${pageJson}...`);
 let page = JSON.parse(jetpack.read(pageJson));
+
+sortChildren(page);
 
 jetpack.write(outPath, JSON.stringify(page, null, '\t'));
