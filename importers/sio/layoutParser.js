ba4cecaa680fea1e7b6a56b8b76ad0e58fd537a3
Vaclav Purchart <vpurchart@kerio.com>
2017-03-14 12:08:35 +0100

    generic SIO layout parser

---
+++ importers/sio/layoutParser.js
@@ -0,0 +1,20 @@
+const jetpack = require('fs-jetpack');
+const columnOrder = JSON.parse(jetpack.read(__dirname + '/../../config.json')).sio.columnOrder;
+
+module.exports = function parseColumns(page) {
+	let columns = {};
+	for (let child of page.children) {
+		let layout = child.layout;
+		columns[layout.column] = columns[layout.column] || [];
+		columns[layout.column][layout.position] = child;
+	}
+
+
+	for (let columnName in columns) {
+		//console.log('layout column: ' + columnName);
+		console.assert(columnOrder.indexOf(columnName) >= 0);
+		//let items = columns[columnName];
+	}
+
+	return columns;
+};
--- importers/sio/sortChildren.js
+++ importers/sio/sortChildren.js
@@ -1,26 +1,17 @@
+const layoutParser = require(__dirname + '/layoutParser');
+const jetpack = require('fs-jetpack');
+const columnOrder = JSON.parse(jetpack.read(__dirname + '/../../config.json')).sio.columnOrder;
+
 function sortChildren(page) {
 	page.children = sortChildrenInternal(page);
 	return page;
 }
 
 function sortChildrenInternal(node) {
-	let columns = {};
-	for (let child of node.children) {
-		let layout = child.layout;
-		columns[layout.column] = columns[layout.column] || [];
-		columns[layout.column][layout.position] = child;
-	}
-
+	let columns = layoutParser(node);
 	let children = [];
-	let knownColumns = ['left', 'center', 'right'];
 
-	for (let columnName in columns) {
-		//console.log('layout column: ' + columnName);
-		console.assert(knownColumns.indexOf(columnName) >= 0);
-		//let items = columns[columnName];
-	}
-
-	for (let columnName of knownColumns) {
+	for (let columnName of columnOrder) {
 		children = children.concat(columns[columnName]);
 	}
 
