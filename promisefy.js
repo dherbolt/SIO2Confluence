8ee00ed3dca958b39723dae7b5baf0c32133ca91
Vaclav Purchart <vpurchart@kerio.com>
2017-03-20 13:54:39 +0100

    async item callbacks can return async results

--- promisefy.js
+++ promisefy.js
@@ -1,4 +1,9 @@
 /**
+ * Call *itemCallback(item)* for each *argList*. When all itemCallbacks are
+ * finished returned Promise is resolved (and optional allDoneCallback is called).
+ *
+ * Item callbacks can return async results as their resolve parameters. These resutls
+ * are collected and returned as allDoneCallback/promisefy:resolve argument.
  *
  * @param {Array} argList List of arguments for each itemCallback call
  * @param {Function} itemCallback - can optionally return Promise
@@ -7,21 +12,23 @@
  */
 const promisefy = function (argList, itemCallback, allDoneCallback) {
 	return new Promise(function (resolve, reject) {
+		var returnValues = [];
 		if (argList.length === 0) {
-			resolve();
+			resolve(returnValues);
 			return;
 		}
 
 		let processItem = function (index, argList, itemCallback, allDoneCallback) {
 			if (index === argList.length) {
-				resolve();
-				allDoneCallback && allDoneCallback();
+				resolve(returnValues);
+				allDoneCallback && allDoneCallback(returnValues);
 				return;
 			}
 
 			let result = itemCallback(argList[index]);
 			if (result instanceof Promise) {
-				result.then(function () {
+				result.then(function (value) {
+					returnValues[index] = value;
 					processItem(index + 1, argList, itemCallback, allDoneCallback);
 				});
 			}
@@ -34,20 +41,53 @@ const promisefy = function (argList, itemCallback, allDoneCallback) {
 	});
 };
 
+
+// --------------------------------------------------------------------------------------
+//                                         <TESTS>
+// --------------------------------------------------------------------------------------
+var testResult;
+
 // Test - async
-// promisefy([1, 2, 3, 4], function (i) {
-// 	return new Promise(function (resolve, reject) {
-// 		console.log(i);
-// 		window.setTimeout(resolve, 500);
-// 	});
-// })
-// .then(() => { console.log('aaaa'); });
+testResult = [];
+promisefy([1, 2, 3, 4], function (i) {
+	return new Promise(function (resolve, reject) {
+		setTimeout(function() {
+			testResult.push(i);
+		}.bind(this, i), 500);
+	});
+})
+.then(() => {
+	console.assert(JSON.stringify([1, 2, 3, 4]) === JSON.stringify(testResult), 'Async promise test');
+});
 
 
 // Test - sync
-// promisefy([1, 2, 3, 4], function (i) {
-// 		console.log(i);
-// })
-// .then(() => { console.log('aaaa'); });
+testResult = [];
+promisefy([1, 2, 3, 4], function (i) {
+	testResult.push(i);
+})
+.then(() => {
+	console.assert(JSON.stringify([1, 2, 3, 4]) === JSON.stringify(testResult), 'Sync promise test');
+});
+
+
+// Test - async with return value
+promisefy([1, 2, 3, 4], function (i) {
+	return new Promise(function (resolve, reject) {
+		setTimeout(function(i) {
+			resolve(i + 10);
+		}.bind(this, i), 500);
+	});
+})
+.then((returnValues) => {
+	console.assert(JSON.stringify([11, 12, 13, 14]) === JSON.stringify(returnValues), 'Async promise with return value test');
+});
+
+
+// --------------------------------------------------------------------------------------
+//                                         </TESTS>
+// --------------------------------------------------------------------------------------
+
+
 
 module.exports = promisefy;
