15049ae9e13e21c2a75b8837f43a61b986973e29
Miroslav Osladil <mosladil@kerio.com>
2017-03-21 09:46:50 +0100

    Fixed regex for parsing pageId

--- common.js
+++ common.js
@@ -2,11 +2,19 @@ const Logger = require('./lib/Logger');
 global.Logger = new Logger('./logs/log-' + (new Date().toISOString().replace(/T/, '_').replace(/\:/g, '-').replace(/\..+/, '')) + '.txt');
 
 global.getSioPageIdFromUrl = function(url) {
-	let parts = url.match(/\/page-(\d+)-?.*/i);
+	let parts = url.match(/\/#?page-(\d+)-?.*/i);
 	if (parts.length === 2) {
 		return parts[1] || null;
 	}
 };
+
+// Test
+console.assert('123' === getSioPageIdFromUrl('https://samepage.io/abcd1234/#page-123'), 'Page ID regex test');
+console.assert('456' === getSioPageIdFromUrl('https://samepage.io/abcd1234/#page-456-neco'), 'Page ID regex test');
+console.assert('789' === getSioPageIdFromUrl('https://samepage.io/abcd1234/#page-789-neco-neco'), 'Page ID regex test');
+console.assert('123' === getSioPageIdFromUrl('https://samepage.io/app/#!/abcd1234/page-123'), 'Page ID regex test');
+console.assert('456' === getSioPageIdFromUrl('https://samepage.io/app/#!/abcd1234/page-456-neco'), 'Page ID regex test');
+console.assert('789' === getSioPageIdFromUrl('https://samepage.io/app/#!/abcd1234/page-789-neco-neco'), 'Page ID regex test');
 
 // for readable callstacks during Promise rejections
 global.Promise = require('bluebird');
