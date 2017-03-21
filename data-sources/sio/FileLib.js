fd99ccccd40c23ec68fd527d3d37387fa9c20254
Vaclav Purchart <vpurchart@kerio.com>
2017-03-21 11:23:09 +0100

    download files

--- data-sources/sio/FileLib.js
+++ data-sources/sio/FileLib.js
@@ -1,12 +1,25 @@
 const sanitize = require(APP_ROOT + '/fileUtil').sanitize;
 const { getNodeInfo } = require(__dirname + '/node');
+const promisefy = require(APP_ROOT + '/promisefy');
+
 
 module.exports = function downloadFileLib(node) {
 	return new Promise(function(resolve, reject) {
 		let nodeInfo = Object.assign({}, getNodeInfo(node), {
 			todo: 'TODO'
 		});
-		// TODO - download directories & files -> resolve
-		resolve(nodeInfo);
+
+		promisefy(node.children, processFile).then(function(children) {
+			nodeInfo.children = children;
+			resolve(nodeInfo);
+		});
 	});
 };
+
+
+function processFile(fileNode) {
+	// TODO - download directories & files -> resolve
+	return new Promise(function (resolve, reject) {
+		resolve(getNodeInfo(fileNode));
+	});
+}
