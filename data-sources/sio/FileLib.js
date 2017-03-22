6ff2fb6a92ba592e7b198e8f6edd7913b403a335
Vaclav Purchart <vpurchart@kerio.com>
2017-03-22 15:20:38 +0100

    subdirectories - finished

--- data-sources/sio/FileLib.js
+++ data-sources/sio/FileLib.js
@@ -11,9 +11,7 @@ module.exports = function downloadFileLib(node, customParams) {
 
 		console.assert(node.type === 'FileLib');
 
-		let nodeInfo = Object.assign({}, getNodeInfo(node, coeId, dirPath), {
-			todo: 'TODO'
-		});
+		let nodeInfo = Object.assign({}, getNodeInfo(node, coeId, dirPath));
 
 		// empty file lib
 		if (!node.children || node.children.length === 0) {
@@ -34,7 +32,7 @@ module.exports = function downloadFileLib(node, customParams) {
 			}
 
 			nodeInfo.children = children;
-			resolve(children);
+			resolve(nodeInfo);
 		});
 	});
 };
@@ -61,29 +59,32 @@ function processFolder(folderNode, args) {
 	//
 	// {"result":{"items":[{"layout":{"position":0},"updatedTime":"2017-03-20T11:14:15+0100","dashifiedName":"gitignore","updatedBy":{"emailAddress":"vpurchart@kerio.com","invited":false,"guid":"6f168fe5c4e56c57d7b13a96030d140def3cafbe","fullName":"Vaclav Purchart","id":"146","type":"user"},"role":"Admin","file":{"size":30,"createdBy":{"emailAddress":"vpurchart@kerio.com","invited":false,"guid":"6f168fe5c4e56c57d7b13a96030d140def3cafbe","fullName":"Vaclav Purchart","id":"146","type":"user"},"name":".gitignore","createdTime":"2017-03-20T11:14:15+0100","version":1,"hash":"6dcd5d83a076885fa575f0c35e6f015f82a3da7a","properties":{}},"createdBy":{"emailAddress":"vpurchart@kerio.com","invited":false,"guid":"6f168fe5c4e56c57d7b13a96030d140def3cafbe","fullName":"Vaclav Purchart","id":"146","type":"user"},"name":".gitignore","createdTime":"2017-03-20T11:14:15+0100","id":"476424173032647918","roleSource":"Member","type":"File"}]},"id":0,"jsonrpc":"2.0"}
 	console.assert(folderNode.type === 'FileFolder');
-	Logger.log('Found folder ' + folderNode.name);
+	Logger.log(`Found folder '${folderNode.name}' expanding files...`);
 
 	return new Promise(function (resolve, reject) {
 		sendXhr('Items.getList', {
 			pid: folderNode.id,
 			sort: [{ "property": "type", "direction": "ASC" }, { "property": "shortName", "direction": "ASC" }, { "property": "extension", "direction": "ASC" }, { "property": "id", "direction": "ASC" }]
 		}, {
-				uri: `${cfg.sio.baseUrl}/${coeId}/server/data?method=Items.getList`
-			}).then(function (args) {
-				let { error, response, body } = args;
+			uri: `${cfg.sio.baseUrl}/${coeId}/server/data?method=Items.getList`
+		}).then(function (result) {
+			let { error, response, body } = result;
 
-				if (body.error) {
-					Logger.error(`ERROR: ID: ${folderNode.id} -- ${JSON.stringify(body.error)}`);
-					Logger.error('Page cannot be downloaded!');
-					throw new Error(JSON.stringify(body.error));
-				}
+			if (body.error) {
+				Logger.error(`ERROR: ID: ${folderNode.id} -- ${JSON.stringify(body.error)}`);
+				Logger.error('Page cannot be downloaded!');
+				throw new Error(JSON.stringify(body.error));
+			}
 
-				let dirFiles = body.result.items;
-				promisefy(dirFiles, processFile, args).then(function (result) {
-					// console.log(result);
-					resolve(result);
-				});
-
+			let dirFiles = body.result.items;
+			Logger.log(`Found ${dirFiles.length} inner files.`);
+			promisefy(dirFiles, processFile, args).then(function (result) {
+				// result = result.map((item) => {
+				// 	return `${sanitize(folderNode.name)}--${item}`;
+				// });
+				resolve(result);
 			});
+
+		});
 	});
 }
