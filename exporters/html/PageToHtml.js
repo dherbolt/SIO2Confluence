bbd1e1e96c64a99afc79893a16d19ce1862ce238
Miroslav Osladil <mosladil@kerio.com>
2017-03-22 10:53:26 +0100

    Moved SIO prefix to config

--- exporters/html/PageToHtml.js
+++ exporters/html/PageToHtml.js
@@ -11,7 +11,7 @@ const cfg = JSON.parse(jetpack.read(APP_ROOT + '/config.json'));
 const linkColor = '#00adef';
 let newSioComponentsHtml = [];
 let cmpIndex = 0;
-const sioIdPrefix = cfg.confluence.sioIdPrefix || 'SIO';
+const sioIdPrefix = cfg.confluence.sioIdPrefix;
 
 module.exports = function processPage (sourceFolder) {
 	const useLayouts = true;
